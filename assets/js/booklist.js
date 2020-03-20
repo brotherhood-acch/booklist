e2f3c459a55c8792e1afdc0b155790fd386bf7c4
lee-han <t01062782363@gmail.com>
2020-03-20 22:33:07 +0900

    start

---
+++ assets/js/booklist.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    var table = $('#booklist').DataTable( {
+				//deferRender: true,
+				buttons: [
+					{extend: 'copy', exportOptions: {modifier: {page: 'current'}}},
+					{extend: 'excel', exportOptions: {modifier: {page: 'current'}}},
+					{extend: 'print', exportOptions: {modifier: {page: 'current'}}},
+					],
+				dom: 'B<"top"lfip>rt<"bottom"ip>',
+				pageLength: 10,
+				lengthMenu: [ 10, 20, 50 ],
+				order: [6, 'asc'],
+				rowGroup: {dataSrc: 6},
+				//stateSave: true,
+				//ajax: "source/booklist.txt",
+        data: dataSet,
+        columns: [
+					{ title: "등록번호" },
+					{ title: "서명" },
+					{ title: "저자" },
+					{ title: "출판사" },
+					{ title: "출판년" },
+					{ title: "총서사항" },
+					{ title: "서가번호", visible: false  },
+					{ title: "주제어" },
+					{ title: "기증자" },
+					{ title: "유형"},
+					{ title: "ISBN/ISSN" },
+					//{ title: "한글검색용색인", visible: false },
+					//{ title: "대표서명", visible: false },
+        ]
+		} );
+		new $.fn.dataTable.FixedHeader( table );
+} );
---
+++ legend.md
@@ -0,0 +1,30 @@
+---
+layout: default
+title: "범례"
+---
+<div class="py-3 text-center" >
+	<div class="container">
+		<div class="row">
+			<div class="mx-auto col-lg-5 col-md-7 col-10">
+				<h1>범례</h1>
+			</div>
+		</div>
+	</div>
+</div>
+
+### 수록 범위
+- 블라블라
+
+### 제목
+- 내용 블라블라
+
+### 총서류 및 중복 자료 처리
+- 블라블라
+
+<br><br>
+* * *
+<br>
+
+### 자료실 서가 배치도
+
+그림그림
